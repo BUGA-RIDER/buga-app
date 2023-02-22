d8dd995afe1de605088ad88c3ca6a003a95da19f
owiniose <owiniose@gmail.com>
2023-02-22 14:43:39 +0100

    react

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/RiderScreens/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+const Stack = createNativeStackNavigator ();
+
+const App = () => {
+  return ( 
+    <NavigationContainer>
+      <Stack.Navigator screenOptions = {{ headerShown: false }} 
+        initialRouteName = "Home">
+          <Stack.Screen name = "Home" component ={Home}/>
+          </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
