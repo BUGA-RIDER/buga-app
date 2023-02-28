724482ca375569526ef7ca125b0077cbc159c157
owiniose <owiniose@gmail.com>
2023-02-28 13:26:06 +0100

    commit

---
+++ components/SubmitDocuments.js
@@ -0,0 +1,234 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Red from "../assets/icons/red_exclamation.svg";
+import Right from "../assets/icons/arrowright.svg";
+import Tick from "../assets/icons/tick.svg";
+
+export const Pending = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#FFE9E9",
+        marginHorizontal: 16,
+        paddingLeft: 13,
+        paddingVertical: 15,
+        alignSelf: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Red
+          style={{
+            marginRight: 8,
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 15,
+          }}
+        >
+          Document Submission pending
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingTop: 5,
+          marginRight: 13,
+          marginLeft: 25,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 14,
+            color: "#4F4F4F",
+          }}
+        >
+          Kindly Submit all required documents to get verified and start
+          earning.
+        </Text>
+
+        <Right width={15} height={15} />
+      </View>
+    </View>
+  );
+};
+
+export const Review = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#DEDEDE",
+        marginHorizontal: 16,
+        paddingLeft: 13,
+        paddingVertical: 15,
+        alignSelf: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Red
+          style={{
+            marginRight: 8,
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 15,
+          }}
+        >
+          Document Submission pending
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingTop: 5,
+          marginRight: 13,
+          marginLeft: 25,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 14,
+            color: "#DEDEDE",
+          }}
+        >
+          Kindly Submit all required documents to get verified and start
+          earning.
+        </Text>
+
+        <Right width={15} height={15} />
+      </View>
+    </View>
+  );
+};
+
+export const Invalid = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#FFE9E9",
+        marginHorizontal: 16,
+        paddingLeft: 13,
+        paddingVertical: 15,
+        alignSelf: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Red
+          style={{
+            marginRight: 8,
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 15,
+          }}
+        >
+          1 or more documents you submitted is invalid
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingTop: 5,
+          marginRight: 13,
+          marginLeft: 25,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 14,
+            color: "#4F4F4F",
+          }}
+        >
+          Kindly follow the instructions for each document and resubmit.{" "}
+        </Text>
+
+        <Right width={15} height={15} />
+      </View>
+    </View>
+  );
+};
+
+export const Congratulations = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#EEFFE9",
+        marginHorizontal: 16,
+        paddingLeft: 13,
+        paddingVertical: 15,
+        alignSelf: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Tick
+          style={{
+            marginRight: 8,
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 15,
+          }}
+        >
+         Congratulations you’re now a verified driver!
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          paddingTop: 5,
+          marginRight: 13,
+          marginLeft: 25,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 14,
+            color: "#4F4F4F",
+          }}
+        >
+          Your documents have been reviewed and found to 
+be valid. Happy driving!
+        </Text>
+
+        <Right width={15} height={15} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
--- screens/DriverScreens/LoggedIn/DriverHome.js
+++ screens/DriverScreens/LoggedIn/DriverHome.js
@@ -1,29 +1,156 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Background from '../../../assets/icons/background.png'
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Background from "../../../assets/icons/background.png";
+import Menu from "../../../assets/icons/Icon.svg";
+import Notification from "../../../assets/icons/Notification.svg";
+import Wallet from "../../../assets/icons/wallet.svg";
+import RightIcon from "../../../assets/icons/arrowright.svg";
+import {
+  Congratulations,
+  Invalid,
+  Pending,
+  Review,
+} from "../../../components/SubmitDocuments";
 
 const DriverHome = () => {
   return (
-    <SafeAreaView>
-      <View>
-      <ImageBackground
-        source={Background}
-        style={styles.background}
-      />
-       </View>
-    
-    </SafeAreaView>
-  )
-}
+    <SafeAreaView
+      style={{
+        backgroundColor: "white",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <ImageBackground source={Background} style={styles.background} />
+      </View>
 
-export default DriverHome
+      <View style={styles.top}>
+        <View style={styles.topIcons}>
+          <Menu />
+        </View>
+        <Text style={styles.topText}>Hi there, Oreoluwa</Text>
+        <View style={styles.topIcons}>
+          <Notification />
+        </View>
+      </View>
+      <View style={styles.walletInfo}>
+        <Wallet
+          style={{
+            alignSelf: "center",
+          }}
+        />
+        <Text style={styles.walletAmount}>₦15,235</Text>
+        <Text style={styles.walletBalance}>WALLET BALANCE</Text>
+
+        <TouchableOpacity style={styles.withdraw}>
+          <Text style={styles.withdrawText}>Withdraw</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          marginTop: 50,
+          marginBottom: 18,
+        }}
+      >
+        <Congratulations />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginHorizontal: 12,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "SatoshiBold",
+            fontSize: 18,
+          }}
+        >
+          Scheduled Pickups
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "SatoshiMedium",
+              fontSize: 16,
+              textDecorationLine: "underline",
+            }}
+          >
+            See all
+          </Text>
+          <RightIcon />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default DriverHome;
 
 const styles = StyleSheet.create({
-  background:{
-    resizeMode:'stretch',
-    width:455,
-    height:270,
-    alignSelf:'center'
-  }
-})
+  background: {
+    resizeMode: "stretch",
+    width: 455,
+    height: 285,
+    alignSelf: "center",
+  },
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 34,
+    marginTop: 20,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  topIcons: {
+    padding: 7,
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  topText: {
+    fontFamily: "SatoshiBold",
+    fontSize: 18,
+  },
+  walletInfo: {
+    alignSelf: "center",
+  },
+  walletAmount: {
+    fontFamily: "SatoshiBold",
+    fontSize: 35,
+  },
+  walletBalance: {
+    fontFamily: "SatoshiBold",
+    fontSize: 13,
+    textAlign: "center",
+    fontWeight: "700",
+    color: "#565656",
+    marginBottom: 15,
+  },
+  withdraw: {
+    alignSelf: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: "white",
+  },
+  withdrawText: {
+    fontFamily: "SatoshiBold",
+    fontSize: 15,
+  },
+});
