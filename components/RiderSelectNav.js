3a6c034844c5ac9c4502ad0e3c0ca769762249d5
owiniose <owiniose@gmail.com>
2023-03-02 13:36:15 +0100

    commit

---
+++ components/RiderSelectNav.js
@@ -0,0 +1,179 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { HeadingText, SubText } from './CustomTextComponent'
+import Mail from "../assets/icons/Mail_icon.svg"
+
+
+const RiderSelectNav = () => {
+  return (
+    <View style={{
+        marginTop:5
+    }}>
+    <View style={{
+        flexDirection:'row',
+        marginBottom:10,
+        justifyContent: 'space-around',
+        padding:10
+    }}>
+
+      <View style={{
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        marginTop: 16
+      }}>
+        <View style={{
+          backgroundColor: "white",
+        }}>
+
+          <Mail style={{
+            marginTop: 6,
+            alignSelf: 'flex-end',
+            marginRight: 8
+          }} />
+          <View style={{
+            marginHorizontal: 24
+          }}>
+            <Mail width={115} height={73} style={{
+            }} />
+          </View>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 18,
+            marginLeft: 10,
+            marginTop: 5
+          }}>Economy</Text>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 12,
+            marginLeft: 10,
+            color: "#A7A7A7",
+            paddingBottom: 8
+          }}>Shared Ride</Text>
+        </View>
+      </View>  
+      <View style={{
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        marginTop: 16
+      }}>
+        <View style={{
+          backgroundColor: "white",
+        }}>
+
+          <Mail style={{
+            marginTop: 6,
+            alignSelf: 'flex-end',
+            marginRight: 8
+          }} />
+          <View style={{
+            marginHorizontal: 24
+          }}>
+            <Mail width={115} height={73} style={{
+            }} />
+          </View>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 18,
+            marginLeft: 10,
+            marginTop: 5
+          }}>Economy</Text>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 12,
+            marginLeft: 10,
+            color: "#A7A7A7",
+            paddingBottom: 8
+          }}>Shared Ride</Text>
+        </View>
+      </View>  
+        
+      </View>
+
+      {/* //second column */}
+
+    <View style={{
+        flexDirection:'row',
+        marginBottom:10,
+        justifyContent: 'space-around',
+        padding:10
+    }}>
+
+      <View style={{
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        marginTop: 16
+      }}>
+        <View style={{
+          backgroundColor: "white",
+        }}>
+
+          <Mail style={{
+            marginTop: 6,
+            alignSelf: 'flex-end',
+            marginRight: 8
+          }} />
+          <View style={{
+            marginHorizontal: 24
+          }}>
+            <Mail width={115} height={73} style={{
+            }} />
+          </View>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 18,
+            marginLeft: 10,
+            marginTop: 5
+          }}>Economy</Text>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 12,
+            marginLeft: 10,
+            color: "#A7A7A7",
+            paddingBottom: 8
+          }}>Shared Ride</Text>
+        </View>
+      </View>  
+      <View style={{
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        marginTop: 16
+      }}>
+        <View style={{
+          backgroundColor: "white",
+        }}>
+
+          <Mail style={{
+            marginTop: 6,
+            alignSelf: 'flex-end',
+            marginRight: 8
+          }} />
+          <View style={{
+            marginHorizontal: 24
+          }}>
+            <Mail width={115} height={73} style={{
+            }} />
+          </View>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 18,
+            marginLeft: 10,
+            marginTop: 5
+          }}>Economy</Text>
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 12,
+            marginLeft: 10,
+            color: "#A7A7A7",
+            paddingBottom: 8
+          }}>Shared Ride</Text>
+        </View>
+      </View>  
+        
+      </View>
+      </View>
+  )
+}
+
+export default RiderSelectNav
+
+const styles = StyleSheet.create({})
--- screens/RiderScreens/LoggedIn/RiderHome.js
+++ screens/RiderScreens/LoggedIn/RiderHome.js
@@ -3,75 +3,64 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import LoggedInHeader from '../../../components/LoggedInHeader'
 import { HeadingText, SubText } from '../../../components/CustomTextComponent'
+import Mail from '../../../assets/icons/Mail_icon.svg'
+import RiderSelectNav from '../../../components/RiderSelectNav'
 
 const RiderHome = () => {
   return (
     <SafeAreaView>
 
-        <LoggedInHeader 
+      <LoggedInHeader
         walletBalance="₦15,235"
         userName="Öreoluwa"
-        bottomText="Top Up"/>
+        bottomText="Top Up" />
 
-        <View style={{
-          flexDirection:'row',
-          marginTop:50,
-          marginHorizontal:49,
-          justifyContent:'space-around'
+      <View style={{
+        flexDirection: 'row',
+        marginTop: 50,
+        marginHorizontal: 49,
+        justifyContent: 'space-around'
+      }}>
+        <TouchableOpacity style={{
+          borderBottomColor: "#FDD247",
+          borderBottomWidth: 2,
+          paddingBottom: 8
         }}>
-          <TouchableOpacity style={{
-            borderBottomColor:"#FDD247",
-            borderBottomWidth:2,
-            paddingBottom:8
-          }}>
           <Text style={{
-            fontFamily:"SatoshiMedium",
-            fontSize:16,
-            
+            fontFamily: "SatoshiMedium",
+            fontSize: 16,
+
           }}>Order Now</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{
-            borderBottomColor:"#FDD247",
-            paddingBottom:8
-          }}>
-          <Text style={{
-            fontFamily:"SatoshiMedium",
-            fontSize:16,
-            
-          }}>Schedule Trip</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{
-                      marginTop:20,
-                      marginLeft:16
-
+        </TouchableOpacity>
+        <TouchableOpacity style={{
+          borderBottomColor: "#FDD247",
+          paddingBottom: 8
         }}>
-          <HeadingText text="Ready To Move?" style={{
-            textAlign:"left",
-            marginBottom:2,
-            fontSize:24
-          }}/>
-          <SubText text="Select Your ride" style={{
-            color:"#A6A2A2",
-            textAlign:"left",
-            fontSize:14
-          }}/>
-        </View>
-            <View style={{
-              marginHorizontal:16,
-              flexDirection:'row',
-              marginTop:16
-            }}>
-                <View style={{
-                  width:148,
-                  height:168,
-                  backgroundColor:"white"
+          <Text style={{
+            fontFamily: "SatoshiMedium",
+            fontSize: 16,
 
-                }}>
+          }}>Schedule Trip</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{
+        marginTop: 20,
+        marginLeft: 16
 
-                </View>
-            </View>
-          </SafeAreaView>
+      }}>
+        <HeadingText text="Ready To Move?" style={{
+          textAlign: "left",
+          marginBottom: 2,
+          fontSize: 24
+        }} />
+        <SubText text="Select Your ride" style={{
+          color: "#A6A2A2",
+          textAlign: "left",
+          fontSize: 14
+        }} />
+      </View>
+      <RiderSelectNav/>
+    </SafeAreaView>
   )
 }
 
